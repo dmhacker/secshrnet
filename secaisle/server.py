056d787c243e03307d7f666475704cc1c891ddc3
David Hacker <dmhacker@protonmail.com>
2020-05-08 18:43:59 -0700

    Add lock to protect host list, better documentation, more packet processing

--- secaisle/server.py
+++ secaisle/server.py
@@ -17,6 +17,7 @@ import comms_pb2
 
 HOST_REFRESH_SECONDS = 1800
 HOST_TTL_SECONDS = 3600
+RECOVER_TIMEOUT_SECONDS = 30
 SOCKET_FILE = '/tmp/secaisle-socket'
 SHARE_DIRECTORY = '{}/.secaisle/shares'.format(os.environ['HOME'])
 
@@ -32,92 +33,33 @@ class Server:
                 raise ValueError("Socket address {} is already in use.")
         self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         self.sock.bind(SOCKET_FILE)
-        self.hid = str(uuid.uuid4())
-        self.hosts = ExpiringDict(max_len=1e10,
-                                  max_age_seconds=HOST_TTL_SECONDS)
         self.redis = redis.Redis(
             host=redis_host,
             port=redis_port,
             password=redis_password)
         self.pubsub = self.redis.pubsub()
+        self.hid = str(uuid.uuid4())
+        self.hosts = ExpiringDict(max_len=1e10,
+                                  max_age_seconds=HOST_TTL_SECONDS)
+        self.hlock = threading.Lock()
         self.received_packets = Queue()
-        # This isn't actually necessary, since this server will see
-        # itself on the broadcast channel when it issues a ping
-        self.hosts[self.hid] = True
+        self.recovered_shares = Queue()
         logger.add("logs/secaisle_{}.log".format(self.hid))
+        logger.info("Our host ID is {}.".format(self.hid))
 
-    def _packet_listener(self):
-        logger.info("Packet listener started.")
-        self.pubsub.subscribe('secaisle:broadcast')
-        self.pubsub.subscribe('secaisle:host:' + self.hid)
-        ping = comms_pb2.Packet()
-        ping.type = comms_pb2.PacketType.IAM
-        ping.sender = self.hid
-        ping.needs_ack = True
-        self.redis.publish('secaisle:broadcast', ping.SerializeToString())
-        for message in self.pubsub.listen():
-            if message['type'] == 'message':
-                packet = comms_pb2.Packet()
-                packet.ParseFromString(message['data'])
-                self.received_packets.put(packet)
-
-    def _packet_processor(self):
-        logger.info("Packet processor started.")
-        while True:
-            packet = self.received_packets.get()
-            logger.info("Received packet {} from host {}.".format(
-                packet.type, packet.sender))
-            if packet.type == comms_pb2.PacketType.IAM:
-                self.hosts[packet.sender] = True
-                if packet.needs_ack:
-                    pong = comms_pb2.Packet()
-                    pong.type = comms_pb2.PacketType.IAM
-                    pong.sender = self.hid
-                    pong.needs_ack = False
-                    self.redis.publish('secaisle:host:' + packet.sender,
-                                       pong.SerializeToString())
-            elif packet.type == comms_pb2.PacketType.STORE:
-                tagpath = os.path.join(SHARE_DIRECTORY, packet.tag)
-                logger.info("Storing share to tag '{}' @ {}."
-                            .format(packet.tag, tagpath))
-                if os.path.exists(tagpath):
-                    logger.warning('{} is being overwritten.'
-                                   .format(tagpath))
-                with open(tagpath, 'wb') as f:
-                    f.write(packet.share.SerializeToString())
-            elif packet.type == comms_pb2.PacketType.RECOVER:
-                # TODO: packet.tag - look for tag in local filesystem
-                pass
-            elif packet.type == comms_pb2.PacketType.RETURN:
-                # TODO: packet.share - send share to command processor
-                pass
-            else:
-                logger.warning("Unable to process unknown packet {}.".format(
-                    packet.type))
-
-    def _keepalive_broadcaster(self):
-        logger.info("Keepalive broadcaster started.")
-        ping = comms_pb2.Packet()
-        ping.type = comms_pb2.PacketType.IAM
-        ping.sender = self.hid
-        ping.needs_ack = False
-        ping_str = ping.SerializeToString()
-        while True:
-            time.sleep(HOST_REFRESH_SECONDS)
-            self.redis.publish('secaisle:broadcast', ping_str)
-
-    def split_shares(self, tag, plaintext, threshold):
+    def split_plaintext(self, tag, plaintext, threshold):
         '''
         :param Server self: The active Server object
         :param bytes plaintext: Plaintext message (in bytes)
         :param str tag: The tag that this message will get filed under
         :param int threshold: Minimum number of shares needed for recreation
         '''
-        if threshold > len(self.hosts):
+        with self.hlock:
+            hkeys = list(self.hosts.keys())
+        if threshold > len(hkeys):
             raise ValueError('Threshold must not be greater than the host count.')
-        shares = crypto.split_shares(plaintext, threshold, len(self.hosts))
-        i = 0
-        for hid in self.hosts.keys():
+        shares = crypto.split_shares(plaintext, threshold, len(hkeys))
+        for i, hid in enumerate(hkeys):
             packet = comms_pb2.Packet()
             packet.type = comms_pb2.PacketType.STORE
             packet.sender = self.hid
@@ -128,21 +70,123 @@ class Server:
             packet.share.ciphertext_hash = shares[i].ciphertext_hash
             self.redis.publish('secaisle:host:' + hid,
                                packet.SerializeToString())
-            i += 1
-        return
 
-    def recover_shares(self, tag):
+    def combine_shares(self, tag):
         '''
         :param Server self: The active Server object
         :param str tag: The tag for the message we want to fetch
         :return: The message if it could be recovered, otherwise None
         :rtype bytes:
         '''
-        # TODO: Broadcast RECOVER packet, aggregate RETURN shares
+        packet = comms_pb2.Packet()
+        packet.type = comms_pb2.PacketType.RECOVER
+        packet.sender = self.hid
+        packet.tag = tag
+        self.redis.publish('secaisle:broadcast', packet.SerializeToString())
+        # TODO: Wait on self.recovered_shares for RETURN shares to come in
         return None
 
+    def _packet_listener(self):
+        '''
+        The packet listener subscribes to two channels on Redis, the broadcast
+        channel and the host's private channel. It will continually poll for
+        packets and forward them to the packet processor.
+
+        Processing is not done on the packet listener's thread. This is because
+        the listener can miss messages if it is not continuously polling.
+        Redis's pub/sub architecture is ephemeral, meaning that it doesn't
+        persist messages. Thus, if a message is missed, the listener can
+        never get it back nor even know about it.
+        '''
+        self.pubsub.subscribe('secaisle:broadcast')
+        self.pubsub.subscribe('secaisle:host:' + self.hid)
+        ping = comms_pb2.Packet()
+        ping.type = comms_pb2.PacketType.IAM
+        ping.sender = self.hid
+        ping.needs_ack = True
+        logger.info("Broadcasting initial discovery ping.")
+        self.redis.publish('secaisle:broadcast', ping.SerializeToString())
+        for message in self.pubsub.listen():
+            if message['type'] == 'message':
+                packet = comms_pb2.Packet()
+                try:
+                    packet.ParseFromString(message['data'])
+                    self.received_packets.put(packet)
+                except Exception:
+                    logger.warn("Unparsable packet on channel {}."
+                                .format(message['channel']))
+
+    def _packet_processor(self):
+        '''
+        The packet processor is responsible for dealing with incoming
+        packets. It will wait on a blocking queue for packets to be
+        inserted from the packet listener. Every time a packet arrives,
+        it will be handled individually and in-order. This may entail
+        response packets being broadcasted back.
+        '''
+        while True:
+            packet = self.received_packets.get()
+            if packet.type == comms_pb2.PacketType.IAM:
+                logger.info("Received ping from host {}.".format(
+                    packet.sender))
+                with self.hlock:
+                    self.hosts[packet.sender] = True
+                if packet.needs_ack:
+                    pong = comms_pb2.Packet()
+                    pong.type = comms_pb2.PacketType.IAM
+                    pong.sender = self.hid
+                    pong.needs_ack = False
+                    self.redis.publish('secaisle:host:' + packet.sender,
+                                       pong.SerializeToString())
+            elif packet.type == comms_pb2.PacketType.STORE:
+                logger.info("Saving share for tag '{}' from host {}."
+                            .format(packet.tag, packet.sender))
+                tagpath = os.path.join(SHARE_DIRECTORY, packet.tag)
+                if os.path.exists(tagpath):
+                    logger.warning('{} is being overwritten.'
+                                   .format(tagpath))
+                with open(tagpath, 'wb') as f:
+                    f.write(packet.share.SerializeToString())
+            elif packet.type == comms_pb2.PacketType.RECOVER:
+                logger.info("Sending share for tag '{}' to host {}."
+                            .format(packet.tag, packet.sender))
+                tagpath = os.path.join(SHARE_DIRECTORY, packet.tag)
+                if os.path.exists(tagpath):
+                    with open(tagpath, 'rb') as f:
+                        response = comms_pb2.Packet()
+                        response.type = comms_pb2.PacketType.RETURN
+                        response.sender = self.hid
+                        response.share.ParseFromString(f.read())
+                        self.redis.publish('secaisle:host:' + packet.sender,
+                                           response.SerializeToString())
+            elif packet.type == comms_pb2.PacketType.RETURN:
+                logger.info("Got a share from host {}."
+                            .format(packet.sender))
+                self.recovered_shares.put(packet.share)
+            else:
+                logger.warning("Unknown packet type {}.".format(
+                    packet.type))
+
+    def _keepalive_broadcaster(self):
+        '''
+        The keepalive broadcaster is responsible for publishing a ping packet
+        at a specified duration. This periodically refreshes the host list for
+        each machine on the network.
+        '''
+        ping = comms_pb2.Packet()
+        ping.type = comms_pb2.PacketType.IAM
+        ping.sender = self.hid
+        ping.needs_ack = False
+        ping_str = ping.SerializeToString()
+        while True:
+            time.sleep(HOST_REFRESH_SECONDS)
+            self.redis.publish('secaisle:broadcast', ping_str)
+
     def _command_processor(self):
-        logger.info("Server is now listening for commands.")
+        '''
+        The command processor is responsible for listening on a UNIX
+        socket and fielding commands from clients who join via the socket.
+        '''
         self.sock.listen(1)
         while True:
             conn, client_addr = self.sock.accept()
@@ -155,28 +199,38 @@ class Server:
                     command.ParseFromString(message)
                     response = comms_pb2.Response()
                     if command.type == comms_pb2.CommandType.NUM_HOSTS:
+                        logger.info("{} sent a NUM_HOSTS command."
+                                    .format(client_addr))
                         response.success = True
-                        response.host_count = len(self.hosts)
+                        with self.hlock:
+                            response.host_count = len(self.hosts)
                     elif command.type == comms_pb2.CommandType.SPLIT:
+                        logger.info("{} sent a SPLIT command for tag '{}'."
+                                    .format(client_addr, command.tag))
                         try:
-                            # Actual plaintext is wrapped in a base64 encoding
-                            plaintext = base64.b64decode(command.plaintext.encode())
-                            self.split_shares(command.tag,
-                                              plaintext,
-                                              command.threshold)
+                            # Received plaintext must be base64-decoded
+                            plaintext = base64.b64decode(
+                                command.plaintext.encode())
+                            self.split_plaintext(command.tag,
+                                                 plaintext,
+                                                 command.threshold)
                             response.success = True
                         except Exception as e:
                             response.success = False
                             response.payload = str(e)
                     elif command.type == comms_pb2.CommandType.COMBINE:
+                        logger.info("{} sent a COMBINE command for tag '{}'."
+                                    .format(client_addr, command.tag))
                         try:
-                            message = self.recover_shares(command.tag)
-                            # Rewrap plaintext in a base64 encoding
+                            message = self.combine_shares(command.tag)
                             response.success = message is not None
                             if response.success:
-                                response.payload = base64.b64encode(message).decode()
+                                # Outgoing plaintext must be base64-encoded
+                                response.payload = base64.b64encode(
+                                    message).decode()
                             else:
-                                response.payload = 'Could not acquire enough shares to reconstruct message.'
+                                response.payload = 'Could not acquire enough \
+                                    shares to reconstruct message.'
                         except Exception as e:
                             response.success = False
                             response.payload = str(e)
@@ -188,6 +242,9 @@ class Server:
                 conn.close()
 
     def run(self):
+        '''
+        Run the server. This will block the calling thread.
+        '''
         threads = []
         threads.append(threading.Thread(target=self._packet_listener,
                                         daemon=True))
